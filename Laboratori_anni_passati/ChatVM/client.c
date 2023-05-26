766d894672cfac52f3b2b48a2f6c755f6dc2ed51
nameisalfio <alfio.spoto@outlook.it>
2023-05-26 16:16:23 +0200

    Complete ChatVM

--- Laboratori_anni_passati/ChatVM/client.c
+++ Laboratori_anni_passati/ChatVM/client.c
@@ -1,12 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <time.h>
 #include <errno.h>
 #include <arpa/inet.h>
 #include <unistd.h>
 
 #define BUFFER_SIZE 1024
+#define MSG_LEN 20
+#define PORT 8000
+#define LANG "C"    //Sostituire con il linguaggio di riferimento della VMClient
+
+typedef struct 
+{
+    int n; 
+    char msg[MSG_LEN];
+    char language[MSG_LEN]; 
+    char IP[16];
+}VMClient;
 
 void handle_error(char* msg)
 {
@@ -16,16 +26,18 @@ void handle_error(char* msg)
 
 int main(int argc, char* argv[])
 {
-    int sockfd, n;
-    struct sockaddr_in addr;
+    int sockfd, udp_sockfd, n;
+    struct sockaddr_in addr, udp_addr;
     socklen_t len = sizeof(struct sockaddr_in);
-    char sendline[BUFFER_SIZE];
-    char recvline[BUFFER_SIZE];
+    char buffer[BUFFER_SIZE];
 
     if(argc != 3)
         handle_error("Error argc\n");
-
+    
     if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+        handle_error("Error socket\n");
+
+    if((udp_sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
         handle_error("Error socket\n");
 
     memset(&addr, 0, len);
@@ -33,29 +45,59 @@ int main(int argc, char* argv[])
     inet_pton(AF_INET, argv[1], &addr.sin_addr);
     addr.sin_port = htons(atoi(argv[2]));
 
-    bzero(sendline, BUFFER_SIZE);
-    bzero(recvline, BUFFER_SIZE);
+    memset(&udp_addr, 0, len);
+    udp_addr.sin_family = AF_INET;
+    udp_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    udp_addr.sin_port = htons(PORT); 
 
-    if((connect(sockfd, (struct sockaddr *)&addr, len)))
+    if((connect(sockfd, (struct sockaddr* )&addr, len)) < 0)
         handle_error("Error connect\n");
+    printf("[+]Connection established\n\n");
 
-    for(;;)
+    if(!fork())
     {
-        printf("Enter a request:\n");
-        fgets(sendline, BUFFER_SIZE, stdin);
-        sendline[strcspn(sendline, "\n")] = 0;
+        if((send(sockfd, LANG, BUFFER_SIZE, 0)) < 0)
+            handle_error("Error send\n");
+        printf("%s\n-------------\n", LANG);
 
-        if(strncmp(sendline, "STOP", strlen("STOP")) == 0)break;
+        VMClient client;
+        strcpy(client.language, LANG);
+        strcpy(client.IP, inet_ntoa(addr.sin_addr));
 
-        if(send(sockfd, sendline, BUFFER_SIZE, 0) < 0)
+        do 
+        {
+            printf("Enter a number between 1 and 6:\n");
+            scanf("%d", &client.n);
+        } while (client.n < 1 || client.n > 6);
+
+        do
+        {
+            printf("\nEnter a message:\n");
+            scanf("%s", &client.msg);
+        } while (strlen(client.msg) >= MSG_LEN);
+
+        if((send(sockfd, &client, sizeof(client), 0)) < 0)
             handle_error("Error send\n");
 
-        if(n = recv(sockfd, recvline, BUFFER_SIZE, 0) < 0)
+        if((n = recv(sockfd, buffer, BUFFER_SIZE, 0)) < 0)
             handle_error("Error recv\n");
-        recvline[n] = 0;
-        printf("Reply: %s, IP: %s, Port: %d\n\n", recvline, inet_ntoa(addr.sin_addr), ntohs(addr.sin_port));
+        buffer[n] = 0;
+        printf("\n[+]VMServer Pascal: %s\n", buffer);
+
+        close(sockfd);
     }
-    close(sockfd);
+    else
+    {
+        if((bind(udp_sockfd, (struct sockaddr *)&udp_addr, len)) < 0)
+            handle_error("Error bind\n");
+
+        if((n = recvfrom(udp_sockfd, buffer, BUFFER_SIZE, 0, (struct sockaddr*)&udp_addr, &len)) < 0)
+            handle_error("Error recvfrom\n");
+        buffer[n] = 0;
+        printf("\nMessage: %s\n", buffer);
+
+        close(udp_sockfd);
+    }
 
     return 0;
 }
--- Laboratori_anni_passati/ChatVM/server.c
+++ Laboratori_anni_passati/ChatVM/server.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <string.h>
 #include <time.h>
 #include <errno.h>
@@ -7,6 +8,54 @@
 #include <unistd.h>
 
 #define BUFFER_SIZE 1024
+#define MSG_LEN 20
+#define MAX_CLIENT 6
+#define PORT 8000
+
+typedef enum 
+{
+    C, CPP, JAVA, PYTHON, R, MATLAB
+} Language;
+
+typedef struct 
+{
+    int n; 
+    char msg[MSG_LEN];
+    char language[MSG_LEN]; 
+    char IP[16];
+}VMClient;
+
+Language toLanguage(char* str) 
+{
+    if (strcmp(str, "C") == 0)
+        return C;
+    else if (strcmp(str, "CPP") == 0)
+        return CPP;
+    else if (strcmp(str, "JAVA") == 0)
+        return JAVA;
+    else if (strcmp(str, "PYTHON") == 0)
+        return PYTHON;
+    else if (strcmp(str, "R") == 0)
+        return R;
+    else if (strcmp(str, "MATLAB") == 0)
+        return MATLAB;
+}
+
+char* toString(Language lang) 
+{
+    if (lang == C)
+        return "C";
+    else if (lang == CPP)
+        return "CPP";
+    else if (lang == JAVA)
+        return "JAVA";
+    else if (lang == PYTHON)
+        return "PYTHON";
+    else if (lang == R)
+        return "R";
+    else if (lang == MATLAB)
+        return "MATLAB";
+}
 
 void handle_error(char* msg)
 {
@@ -14,13 +63,129 @@ void handle_error(char* msg)
     exit(EXIT_FAILURE);
 }
 
+int** getMatrix(int a, int b)
+{
+    int ** mat = malloc(sizeof(int*) * MAX_CLIENT);
+    for(int i=0; i<MAX_CLIENT; i++)
+    {
+        mat[i] = malloc(sizeof(int) * MAX_CLIENT);
+        for(int j=0; j<=i; j++)
+        {
+            int r = rand()%(b-a+1)+a;
+            if(i == j)
+                r = 0;
+            mat[i][j] = mat[j][i] = r;
+        }
+    }
+    return mat;
+}
+
+void printMatrix(int** mat)
+{
+    printf("Matrix\n\n");
+    for(int i=0; i<MAX_CLIENT; i++)
+    {
+        for(int j=0; j<MAX_CLIENT; j++)
+            printf("%d ", mat[i][j]);
+        printf("\n");
+    }
+    printf("\n");
+}
+
+void writeFile(char* buffer, char* ip_sender)
+{
+    FILE* fp;
+    if (!(fp = fopen("VMClients.txt", "a+")))
+        handle_error("Error fopen\n");
+
+    fseek(fp, 0, SEEK_SET);
+
+    char line[BUFFER_SIZE];
+    while (fgets(line, sizeof(line), fp) != NULL)
+    {
+        if (strncmp(line, buffer, strlen(buffer)) == 0)  //se è già presente
+        {
+            fclose(fp);
+            return;  
+        }
+    }
+
+    fprintf(fp, "%s %s\n", buffer, ip_sender);
+    fflush(fp);
+
+    fclose(fp);
+}
+
+void readFile(int index, char** ip)
+{
+    FILE* fp;
+    if (!(fp = fopen("VMClients.txt", "r")))
+        handle_error("Error fopen\n");
+
+    char line[BUFFER_SIZE];
+    while (fgets(line, BUFFER_SIZE, fp))
+    {
+        char* tok = strtok(line, " ");
+        if (toLanguage(tok) == index)
+        {
+            tok = strtok(NULL, "\n");
+            *ip = malloc(strlen(tok) + 1);  // Importante
+            strncpy(*ip, tok, strlen(tok));  
+            break;
+        }
+    }
+
+    fclose(fp);
+}
+
+char** find_closest_languages(VMClient* client, int** distanceMatrix) 
+{
+    int n = client->n;
+    int id = toLanguage(client->language);
+    int *row = distanceMatrix[id];
+    int index[MAX_CLIENT];
+
+    //Scrivo nell'array degli indici tutti gli indici a meno di id
+    int currentIndex = 0;
+    for (int i = 0; i < MAX_CLIENT; i++) 
+    {
+        if (i != id) index[currentIndex++] = i;
+    }
+
+    //Bubble sort 
+    for (int i = 0; i < currentIndex - 1; i++) 
+    {
+        for (int j = i + 1; j < currentIndex; j++) 
+        {
+            if (row[index[j]] < row[index[i]]) 
+            {
+                int temp = index[i];
+                index[i] = index[j];
+                index[j] = temp;
+            }
+        }
+    }
+
+    printf("\nI %d linguaggi più vicini a %s sono ", n, client->language);
+    for(int i = 0; i < n; i++)
+        printf("%s ", toString(index[i]));
+    printf("\n\n");
+    
+    char** closest = malloc(sizeof(char*) * n);
+    for(int i = 0; i < n; i++)
+        readFile(index[i], &closest[i]);
+    
+    return closest;
+}
+
 int main(int argc, char* argv[])
 {
     int sockfd, new_sock, n;
     struct sockaddr_in server_addr, client_addr;
     socklen_t len = sizeof(struct sockaddr_in);
-    char sendline[BUFFER_SIZE];
-    char recvline[BUFFER_SIZE];
+    char buffer[BUFFER_SIZE];
+    int** distanceMatrix = getMatrix(10, 255);
+    printMatrix(distanceMatrix);
 
     if(argc != 2)
         handle_error("Error argc\n");
@@ -32,9 +197,6 @@ int main(int argc, char* argv[])
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
     server_addr.sin_port = htons(atoi(argv[1]));
-
-    bzero(sendline, BUFFER_SIZE);
-    bzero(recvline, BUFFER_SIZE);
 
     if((bind(sockfd, (struct sockaddr *)&server_addr, len)) < 0)
         handle_error("Error bind\n");
@@ -52,18 +214,38 @@ int main(int argc, char* argv[])
         {
             for(;;)
             {
-                if((n = recv(new_sock, recvline, BUFFER_SIZE, 0)) < 0)
+                if((n = recv(new_sock, buffer, BUFFER_SIZE, 0)) < 0)
                     handle_error("Error recv\n");
-                if(n == 0)
-                {
-                    printf("\n[+]Client %s disconnected to port %d\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
-                    break;
-                }
-                recvline[n] = 0;
-                printf("\nRequest: %s, IP: %s, Port: %d\n", recvline, inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
-                strcpy(sendline, recvline);
-                if((send(new_sock, sendline, BUFFER_SIZE, 0)) < 0)
+                writeFile(buffer, inet_ntoa(client_addr.sin_addr));
+
+                if((n = recv(new_sock, buffer, BUFFER_SIZE, 0)) < 0)
+                    handle_error("Error recv\n");
+
+                VMClient client;
+                memcpy(&client, buffer, sizeof(client));
+  
+                printf("\nMessage: %s, IP: %s, Port: %d\n", client.msg, inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+                if((send(new_sock, "ACK", BUFFER_SIZE, 0)) < 0)
                     handle_error("Error send\n");
+                
+                char** closest = find_closest_languages(&client, distanceMatrix);
+                for(int i=0; i<client.n; i++)
+                {
+                    int client_sock;
+                    struct sockaddr_in dest_addr;
+
+                    memset(&dest_addr, 0, len);
+                    dest_addr.sin_family = AF_INET;
+                    dest_addr.sin_addr.s_addr = inet_addr(closest[i]);
+                    dest_addr.sin_port = htons(PORT);
+
+                    if((client_sock = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
+                        handle_error("Error socket\n");
+
+                    sendto(client_sock, client.msg, MSG_LEN, 0, (struct sockaddr*)&dest_addr, len);
+
+                    close(client_sock);
+                }
             }
             close(new_sock);
         }
