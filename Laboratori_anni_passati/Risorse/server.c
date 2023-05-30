72b7c6e7ef067997d7a9186511e034c74ec2ab0b
nameisalfio <alfio.spoto@outlook.it>
2023-05-30 22:57:53 +0200

    Operation_3

--- Laboratori_anni_passati/Risorse/server.c
+++ Laboratori_anni_passati/Risorse/server.c
@@ -26,7 +26,7 @@ typedef struct
 {
     char name[NAME_SIZE];   
     int amount;
-    int ID[10];
+    int* ID_list;
 }Resource;
 
 void printRequest(Request* request)
@@ -38,7 +38,7 @@ void printRequest(Request* request)
 
 char* operation1(Request* request)
 {
-    // RAM:3:0:0:0 
+    // RAM:3:4:2
     FILE* fp;
     if(!(fp = fopen("Database.txt", "r+")))
         handle_error("Error fopen\n");
@@ -64,7 +64,7 @@ char* operation1(Request* request)
 
 char* operation2(Request* request)
 {
-    // RAM:3:0:0:0 
+    // RAM:3:4:2
     FILE* fp;
     if(!(fp = fopen("Database.txt", "r+")))
         handle_error("Error fopen\n");
@@ -89,7 +89,54 @@ char* operation2(Request* request)
 
 char* operation3(Request* request)
 {
-    // RAM:3:0:0:0 
+    // RAM:3:4:2
+    /*
+    CONTENUTO DEL FILE DI TESTO --> <nome>:<quantità_residua>:<client_1>:...:<client_n>
+
+        RAM:5
+        CPU:4:3
+        HARD-DISK:1
+        GPU:0
+        DRAM:2
+    */
+    FILE* fp;
+    if(!(fp = fopen("Database.txt", "r+")))
+        handle_error("Error fopen\n");
+
+    Resource resource;
+    char* buffer = malloc(BUFFER_SIZE);
+    char line[BUFFER_SIZE];
+
+    //  3)Chiedere l’elenco delle risorse già prenotate;
+
+    while(fgets(line, BUFFER_SIZE, fp))
+    {
+        strcpy(resource.name, strtok(line, ":"));
+        resource.amount = atoi(strtok(NULL, ":"));
+        resource.ID_list = malloc(sizeof(int)*resource.amount);
+        
+        int n=0;
+        char* str;
+        while((str = strtok(NULL, ":")))    //numero non noto a priori di client
+            resource.ID_list[n++] = atoi(str);  
+
+        for(int i=0; i<n; i++)
+        {
+            printf("%d", resource.ID_list[i]);
+            if(resource.ID_list[i] == request->ID)
+            {
+                strcat(buffer, resource.name);
+                strcat(buffer, " ");
+            }
+        }
+    }
+    fclose(fp);
+    return buffer;
+}
+
+/*
+char* operation3(Request* request)
+{
     FILE* fp;
     if(!(fp = fopen("Database.txt", "r+")))
         handle_error("Error fopen\n");
@@ -103,14 +150,33 @@ char* operation3(Request* request)
     while(fgets(line, BUFFER_SIZE, fp))
     {
 
+        strcpy(resource.name, strtok(line, ":"));
+        resource.amount = atoi(strtok(NULL, ":"));
+        resource.ID_list = malloc(sizeof(int)*resource.amount);
+        for(int i=0; i<resource.amount-1; i++){
+            char str[BUFFER_SIZE];
+            sprintf(str, "%s ", strtok(NULL, ":"));
+            strcat(buffer, str);
+            //resource.ID_list[i] = atoi(strtok(NULL, ":"));  //SEGMENTATION FAULT
+        }
+        return buffer;
+        for(int i=0; i<resource.amount; i++)
+        {
+            if(resource.ID_list[i] == request->ID)
+            {
+                strcat(buffer, resource.name);
+                strcat(buffer, " ");
+            }
+        }
     }
     fclose(fp);
     return buffer;
 }
+*/
 
 char* operation4(Request* request)
 {
-    // RAM:3:0:0:0 
+    // RAM:3:4:2
     FILE* fp;
     if(!(fp = fopen("Database.txt", "r+")))
         handle_error("Error fopen\n");
